3d8c35df3c3a08bf50cc894f1fdc9267eb5ec486
niharika <41866167+SachdevaSaab@users.noreply.github.com>
2018-12-17 13:38:43 -0500

    added barchart

--- Barchart/bargraph.js
+++ Barchart/bargraph.js
@@ -1,15 +1,17 @@
 // Traces
-console.log(ds);
+console.log(regiondata);
 
 var trace1 = {
-    x : ds.map( x => x.Happiness_Score),
-    y : ds.map( x => x.Region),
+    // x : ds.map( x => x.Happiness_Score),
+    // y : ds.map( x => x.Region),
+    x : regiondata.map( x => x.Region),
+    y : regiondata.map( x => x.Happiness_meanscore),
     mode: "markers",
-    orientation: 'h',
+    // orientation: 'h',
     type: "bar",
     name : "GDP",
     marker: {
-        color: "orange",
+        color: "red",
         width: 1
         // size : 9,
         // opacity: 0.4        
@@ -37,9 +39,27 @@ console.log(data);
 // Define the plot layout
 var layout = {
   title: "Happiness Score vs Region",
-  xaxis: { title: "Happiness Score"},    
-  yaxis: { title: "World Region",tickangle: -45 }
+  xaxis: { title: "World Region"},    
+  yaxis: { title: "Happiness_Score",tickangle: -45,
+  }
 };
 
 // Plot - div tag with id "plot"
 Plotly.newPlot("plot", data, layout);
+
+// d3.csv("source-data.csv", function(error, csv_data) {
+//   var data = d3.nest()
+//    .key(function(d) { return d.date;})
+//    .rollup(function(d) { 
+//     return d3.sum(d, function(g) {return g.value; });
+//    }).entries(csv_data);
+//  ...
+//  });
+// var nest = d3.nest()
+//   .key(function(d) { return d.region; })
+//   .sortKeys(d3.ascending)
+//   .rollup(function(d) {
+//     return {
+//       meanVakValue: d3.mean(d, function(g) { return g[value]; })
+//     };
+//   })
