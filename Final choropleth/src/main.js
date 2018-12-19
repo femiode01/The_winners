c7f91b8aae5288fa1a3e8d7b92f355f62f74bbdd
Payedas7 <payedas7@outlook.com>
2018-12-19 01:04:04 -0500

    choropleth with legend

--- Final choropleth/src/main.js
+++ Final choropleth/src/main.js
@@ -13,42 +13,29 @@ var populationById = {};
  * @param d
  * @returns {string}
  */
- 
- function getColor(d) {
-    return d > 150 ? '#800026' :
-           d > 130  ? '#BD0026' :
-           d > 110  ? '#E31A1C' :
-           d > 90  ? '#FC4E2A' :
-           d > 70   ? '#FD8D3C' :
-           d > 50   ? '#FEB24C' :
-           d > 30   ? '#FED976' :
-                      '#FFEDA0';
-}
-
 function getColor1(d) {
 	//if (year === "2015"){ a = '#4d004b'; b= '#8c6bb1';c ='#8c96c6'; d='#9ebcda';e='#bfd3e6'; f='#bfd3e6'; g='#F7FCFD'};
 	//if (year === "2016"){ a = '#edf8b1'; b= '#7fcdbb';c ='#e5f5e0'; d='#a1d99b';e='#f7fcb9'; f='#addd8e' ; g='#31a354'};
 	//if (year === "2017"){ a = '#f03b20'; b= '#fff7bc';c ='#fec44f'; d='#d95f0e';e='#ffeda0'; f='#feb24c'; g='#31a354'};
     return d > 150 ? '#4d004b':
         d > 130 ? '#8c6bb1' :
-            d > 110 ? '#8c96c6' :
-                d > 90 ? '#9ebcda' :
-                    d > 70 ? '#bfd3e6' :
-                        d > 50 ? '#bfd3e6' :
-                            d > 30 ? '#F7FCFD':
-                                '#FF0000';
+            d > 120 ? '#8c96c6' :
+                d > 100 ? '#9ebcda' :
+                    d > 80 ? '#bfd3e6' :
+                        d > 40 ? '#bfd3e6' :
+                            d > 20 ? '#F7FCFD':
+                                'grey';
 }
 
 function getColor2(d, year) {
-    if (year === "2016"){    return d > 150 ? '#0B5345' :
-        d > 130 ? '#196F3D' :
-            d > 120 ? '#239B56' :
-                d > 100 ? '#138D75' :
-                    d > 80 ? '#27AE60' :
-                        d > 40 ? '#58D68D' :
-                            d > 20 ? '#73C6B6' :
-                                d > 0 ? '#ABEBC6' :
-                                '#EAFAF1';};
+    if (year === "2016"){    return d > 150 ? '#edf8b1' :
+        d > 130 ? '#7fcdbb' :
+            d > 120 ? '#e5f5e0' :
+                d > 100 ? '#a1d99b' :
+                    d > 80 ? '#f7fcb9' :
+                        d > 40 ? '#addd8e' :
+                            d > 20 ? '#31a354' :
+                                'grey';};
 	if (year === "2017"){ return d > 150 ? '#f03b20' :
         d > 130 ? '#fff7bc' :
             d > 120 ? '#fec44f' :
@@ -56,17 +43,15 @@ function getColor2(d, year) {
                     d > 80 ? '#ffeda0' :
                         d > 40 ? '#feb24c' :
                             d > 20 ? '#31a354' :
-                                d > 0 ? '#B6D89B' :
-                                '#FF0000';};
+                                'grey';};
 	if (year === "2015"){  return d > 150 ? '#4d004b':
         d > 130 ? '#8c6bb1' :
             d > 120 ? '#8c96c6' :
                 d > 100 ? '#9ebcda' :
                     d > 80 ? '#bfd3e6' :
-                        d > 40 ? '#B9bbbe' :
-                            d > 20 ? '#B9bbbe':
-                                d > 0 ? '#F7fcfd' :
-                                'black';};
+                        d > 40 ? '#bfd3e6' :
+                            d > 20 ? '#F7FCFD':
+                                'grey';};
 
     
 }
@@ -80,7 +65,7 @@ function getColor3(d) {
                     d > 80 ? '#ffeda0' :
                         d > 40 ? '#feb24c' :
                             d > 20 ? '#31a354' :
-                                '#FF0000';
+                                'grey';
 }
 
 // var tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -93,13 +78,12 @@ var tileLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     id: 'mapbox.streets'
 }).addTo(map);
 
-var _r = "2015";
+var _r;
 var filval = "src/data/happy.json";
-
 
 function sndForm()
 {
-//	alert("hello");
+//	alert("vibhaw");
 //row = document.getElementsByName("selector")[0].value;
 row = document.getElementById("selector");
 _r = row.options[row.selectedIndex].value;
@@ -109,7 +93,7 @@ if (_r === "2016") { filval = "src/data/happy2.json"};
 if (_r === "2017") { filval = "src/data/happy3.json"};
 
 //alert(filval);
-//alert("hello");
+//alert("hello vibhaw");
 var xhr = new XMLHttpRequest();				
 //xhr.open('GET', sublayer);
 xhr.open('GET', filval);
@@ -155,7 +139,7 @@ xhr1.onload = function () {
                     fillColor: (total ? getColor2(total,_r) : getColor2(0, _r)),
                     fillOpacity: 0.8,
                     weight: 1,
-                    color: 'red'
+                    color: 'grey'
                 };
             },
             onEachFeature: function (feature, layer) {
@@ -185,44 +169,6 @@ xhr1.onload = function () {
                 });
             }
         }).addTo(map);
-	    try {
-            var list = d3.getElementsByClassName('info legend');
-            list.parentNode.removeChild(list.parentNode.childNode);
-        }
-        catch(err) {
-            console.log(err);
-        };
-		 
-		 var legend = L.control({ position: 'bottomright' })
-		 legend.remove();
-		 
-		 var legend = L.control({ position: 'bottomright' })
-		 //delete map.legend;
-         //var legend = L.control({ position: 'bottomright' })
-		 legend.onAdd = function (map) {
-          
-         var div = L.DomUtil.create('div', 'info legend'),
-         
-		 grades = [0, 20, 40, 80, 100, 120, 130, 150],
-		 labels = [],
-		 from, to;
-
-	     for (var i = 0; i < grades.length; i++) {
-		 from = grades[i];
-		 to = grades[i + 1];
-
-		 labels.push(
-		     '<i style="background:' + getColor2(from + 1,_r) + '"></i> ' +
-		     from + (to ? '&ndash;' + to : '+'));
-	     }
-
-	     div.innerHTML = labels.join('<br>');
-		 //div.innerHTML = '<select><option>1</option><option>2</option><option>3</option></select>';
-		 //div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
-	     return div;
-	 };
-
-	 legend.addTo(map);
 
     }
 };
@@ -231,7 +177,7 @@ xhr1.send();
 };
 
 //alert(filval);
-//alert("hello");
+//alert("hello vibhaw");
 var xhr = new XMLHttpRequest();				
 //xhr.open('GET', sublayer);
 xhr.open('GET', filval);
@@ -254,7 +200,7 @@ xhr.send();
 
 var xhr1 = new XMLHttpRequest();
 //xhr1.open('GET', "src/data/50m.json");
-xhr1.open('GET', "src   /data/world.geo.json");
+xhr1.open('GET', "src/data/world.geo.json");
 xhr1.setRequestHeader('Content-Type', 'application/json');
 xhr1.onload = function () {
     if (xhr1.status === 200) {
@@ -269,7 +215,7 @@ xhr1.onload = function () {
                     fillColor: (total ? getColor1(total) : getColor1(0)),
                     fillOpacity: 0.8,
                     weight: 1,
-                    color: 'black'
+                    color: 'grey'
                 };
             },
             onEachFeature: function (feature, layer) {
--- choropleth_final_18/src/js/main.js
+++ choropleth_final_18/src/js/main.js
@@ -6,7 +6,7 @@ var map = L.map('map', {
 
 // var map = L.map('map').setView([37.8, -96], 4);
 
-var populationById = {};
+var happinessById = {};
 
 /**
  * Returns color based on population.
@@ -36,37 +36,37 @@ function getColor1(d) {
                     d > 70 ? '#bfd3e6' :
                         d > 50 ? '#bfd3e6' :
                             d > 30 ? '#F7FCFD':
-                                '#FF0000';
+                                'grey';
 }
 
 function getColor2(d, year) {
-    if (year === "2016"){    return d > 150 ? '#0B5345' :
-        d > 130 ? '#196F3D' :
-            d > 120 ? '#239B56' :
-                d > 100 ? '#138D75' :
-                    d > 80 ? '#27AE60' :
-                        d > 40 ? '#58D68D' :
-                            d > 20 ? '#73C6B6' :
-                                d > 0 ? '#ABEBC6' :
-                                '#EAFAF1';};
-	if (year === "2017"){ return d > 150 ? '#f03b20' :
-        d > 130 ? '#fff7bc' :
-            d > 120 ? '#fec44f' :
-                d > 100 ? '#d95f0e' :
-                    d > 80 ? '#ffeda0' :
-                        d > 40 ? '#feb24c' :
-                            d > 20 ? '#31a354' :
-                                d > 0 ? '#B6D89B' :
-                                '#FF0000';};
+    if (year === "2016"){    return d > 150 ? '#0b5345' :
+        d > 130 ? '#196f3d' :
+            d > 120 ? '#239b56' :
+                d > 100 ? '#138d75' :
+                    d > 80 ? '#27ae60' :
+                        d > 40 ? '#58868d' :
+                            d > 20 ? '#73c6b6' :
+							 d > 0 ? '#ABEBC6' :
+                                '#F5EEF8';};
+	if (year === "2017"){ return d > 150 ? '#800026' :
+        d > 130 ? '#BD0026' :
+            d > 120 ? '#E31A1C' :
+                d > 100 ? '#FC4E2A' :
+                    d > 80 ? '#FD8D3C' :
+                        d > 40 ? '#FEB24C' :
+                            d > 20 ? '#FED976' :
+							d > 0 ? '#FFEDA0' :
+                                '#F5EEF8';};
 	if (year === "2015"){  return d > 150 ? '#4d004b':
         d > 130 ? '#8c6bb1' :
-            d > 120 ? '#8c96c6' :
-                d > 100 ? '#9ebcda' :
-                    d > 80 ? '#bfd3e6' :
-                        d > 40 ? '#B9bbbe' :
-                            d > 20 ? '#B9bbbe':
-                                d > 0 ? '#F7fcfd' :
-                                'black';};
+            d > 120 ? '#162e45' :
+                d > 100 ? '#2c4e6f' :
+                    d > 80 ? '#49739a' :
+                        d > 40 ? '#7399bd' :
+                            d > 20 ? '#a0b9d2':
+							d > 0 ? '#d3e0ec' :
+                                '#F5EEF8';};
 
     
 }
@@ -80,10 +80,10 @@ function getColor3(d) {
                     d > 80 ? '#ffeda0' :
                         d > 40 ? '#feb24c' :
                             d > 20 ? '#31a354' :
-                                '#FF0000';
+                                'grey';
 }
 
-// var tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+
 var tileLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     noWrap: true,
     maxZoom: 18,
@@ -99,26 +99,24 @@ var filval = "src/data/happy.json";
 
 function sndForm()
 {
-//	alert("hello");
-//row = document.getElementsByName("selector")[0].value;
+
 row = document.getElementById("selector");
 _r = row.options[row.selectedIndex].value;
-//alert(_r);
+
 if (_r === "2015") { filval = "src/data/happy.json"};
 if (_r === "2016") { filval = "src/data/happy2.json"};
 if (_r === "2017") { filval = "src/data/happy3.json"};
 
-//alert(filval);
-//alert("hello");
+
 var xhr = new XMLHttpRequest();				
-//xhr.open('GET', sublayer);
+
 xhr.open('GET', filval);
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.onload = function () {
     if (xhr.status === 200) {
         var population = JSON.parse(xhr.responseText);
         population.forEach(function (d) {
-            populationById[d.Country] = {
+            happinessById[d.Country] = {
                 total: +d.total,
                 Happiness_Rank: +d.Happiness_Rank,
                 Happiness_Score: +d.Happiness_Score
@@ -130,32 +128,25 @@ xhr.onload = function () {
 xhr.send();
 
 var xhr1 = new XMLHttpRequest();
-//xhr1.open('GET', "src/data/50m.json");
+
 xhr1.open('GET', "src/data/world.geo.json");
 xhr1.setRequestHeader('Content-Type', 'application/json');
 xhr1.onload = function () {
     if (xhr1.status === 200) {
         var world = JSON.parse(xhr1.responseText);
-		//alert(_r);
-        //var worldGeoJSON = topojson.feature(world, world.objects.countries).features;
+
         var geoJsonLayer = L.geoJson(world, {
             style: function (feature) {
                 var country = feature.properties.name;
-                total = populationById[country] && populationById[country]["Happiness_Rank"];
-				//alert(_r);
+                total = happinessById[country] && happinessById[country]["Happiness_Rank"];
+
 
                 return {
-					//if (_r === "2015") {
-					//fillColor: (total ? getColor1(total) : getColor1(0)),fillOpacity: 0.8,weight: 1, color: 'grey'};
-					//if (_r === "2016") {
-					//fillColor: (total ? getColor2(total) : getColor2(0)),fillOpacity: 0.8,weight: 1, color: 'grey'};
-					//if (_r === "2017") {
-					//fillColor: (total ? getColor3(total) : getColor3(0)),fillOpacity: 0.8,weight: 1, color: 'grey'};
 					
                     fillColor: (total ? getColor2(total,_r) : getColor2(0, _r)),
                     fillOpacity: 0.8,
                     weight: 1,
-                    color: 'red'
+                    color: 'grey'
                 };
             },
             onEachFeature: function (feature, layer) {
@@ -169,8 +160,8 @@ xhr1.onload = function () {
                         details = feature.properties;
                         country = details.name;
                         document.getElementsByClassName("country")[0].innerHTML = country;
-                        document.getElementsByClassName("Score")[0].innerHTML = "Happiness_Score " + (populationById[country] ? populationById[country].Happiness_Score : "¯\\_(ツ)_/¯");
-                        document.getElementsByClassName("Rank")[0].innerHTML = "Happiness_Rank " + (populationById[country] ? populationById[country].Happiness_Rank : "¯\\_(ツ)_/¯");
+                        document.getElementsByClassName("Score")[0].innerHTML = "Happiness_Score " + (happinessById[country] ? happinessById[country].Happiness_Score : "¯\\_(ツ)_/¯");
+                        document.getElementsByClassName("Rank")[0].innerHTML = "Happiness_Rank " + (happinessById[country] ? happinessById[country].Happiness_Rank : "¯\\_(ツ)_/¯");
                         document.getElementsByClassName("details")[0].style.visibility = 'visible';
                     },
                     'mouseout': function (e) {
@@ -185,62 +176,26 @@ xhr1.onload = function () {
                 });
             }
         }).addTo(map);
-	    try {
-            var list = d3.getElementsByClassName('info legend');
-            list.parentNode.removeChild(list.parentNode.childNode);
-        }
-        catch(err) {
-            console.log(err);
-        };
+	    
+	
 		 
-		 var legend = L.control({ position: 'bottomright' })
-		 legend.remove();
 		 
-		 var legend = L.control({ position: 'bottomright' })
-		 //delete map.legend;
-         //var legend = L.control({ position: 'bottomright' })
-		 legend.onAdd = function (map) {
-          
-         var div = L.DomUtil.create('div', 'info legend'),
-         
-		 grades = [0, 20, 40, 80, 100, 120, 130, 150],
-		 labels = [],
-		 from, to;
-
-	     for (var i = 0; i < grades.length; i++) {
-		 from = grades[i];
-		 to = grades[i + 1];
-
-		 labels.push(
-		     '<i style="background:' + getColor2(from + 1,_r) + '"></i> ' +
-		     from + (to ? '&ndash;' + to : '+'));
-	     }
-
-	     div.innerHTML = labels.join('<br>');
-		 //div.innerHTML = '<select><option>1</option><option>2</option><option>3</option></select>';
-		 //div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
-	     return div;
-	 };
-
-	 legend.addTo(map);
-
-    }
+    };
 };
 xhr1.send();
 
 };
 
-//alert(filval);
-//alert("hello");
+
 var xhr = new XMLHttpRequest();				
-//xhr.open('GET', sublayer);
+
 xhr.open('GET', filval);
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.onload = function () {
     if (xhr.status === 200) {
         var population = JSON.parse(xhr.responseText);
         population.forEach(function (d) {
-            populationById[d.Country] = {
+            happinessById[d.Country] = {
                 total: +d.total,
                 Happiness_Rank: +d.Happiness_Rank,
                 Happiness_Score: +d.Happiness_Score
@@ -253,23 +208,23 @@ xhr.send();
 
 
 var xhr1 = new XMLHttpRequest();
-//xhr1.open('GET', "src/data/50m.json");
-xhr1.open('GET', "src   /data/world.geo.json");
+
+xhr1.open('GET', "src/data/world.geo.json");
 xhr1.setRequestHeader('Content-Type', 'application/json');
 xhr1.onload = function () {
     if (xhr1.status === 200) {
         var world = JSON.parse(xhr1.responseText);
-        //var worldGeoJSON = topojson.feature(world, world.objects.countries).features;
+
         var geoJsonLayer = L.geoJson(world, {
             style: function (feature) {
                 var country = feature.properties.name;
-                total = populationById[country] && populationById[country]["Happiness_Rank"];
+                total = happinessById[country] && happinessById[country]["Happiness_Rank"];
 
                 return {
                     fillColor: (total ? getColor1(total) : getColor1(0)),
                     fillOpacity: 0.8,
                     weight: 1,
-                    color: 'black'
+                    color: 'grey'
                 };
             },
             onEachFeature: function (feature, layer) {
@@ -283,8 +238,8 @@ xhr1.onload = function () {
                         details = feature.properties;
                         country = details.name;
                         document.getElementsByClassName("country")[0].innerHTML = country;
-                        document.getElementsByClassName("Score")[0].innerHTML = "Happiness_Score " + (populationById[country] ? populationById[country].Happiness_Score : "¯\\_(ツ)_/¯");
-                        document.getElementsByClassName("Rank")[0].innerHTML = "Happiness_Rank " + (populationById[country] ? populationById[country].Happiness_Rank : "¯\\_(ツ)_/¯");
+                        document.getElementsByClassName("Score")[0].innerHTML = "Happiness_Score " + (happinessById[country] ? happinessById[country].Happiness_Score : "¯\\_(ツ)_/¯");
+                        document.getElementsByClassName("Rank")[0].innerHTML = "Happiness_Rank " + (happinessById[country] ? happinessById[country].Happiness_Rank : "¯\\_(ツ)_/¯");
                         document.getElementsByClassName("details")[0].style.visibility = 'visible';
                     },
                     'mouseout': function (e) {
@@ -299,44 +254,104 @@ xhr1.onload = function () {
                 });
             }
         }).addTo(map);
-		
-
-	 function style(feature) {
-	     return {
-		 weight: 2,
-		 opacity: 1,
-		 color: 'white',
-		 dashArray: '3',
-		 fillOpacity: 0.7,
-		 fillColor: getColor(feature.properties.Happiness_Rank)
-	     };
-	 }
 	 
-		 var legend = L.control({ position: 'bottomright' })
-		 legend.onAdd = function (map) {
+			function style(feature) {
+				return {
+				weight: 2,
+				opacity: 1,
+				color: 'white',
+				dashArray: '3',
+				fillOpacity: 0.7,
+				fillColor: getColor(feature.properties.Happiness_Rank)
+				};
+			};
 
-	     var div = L.DomUtil.create('div', 'info legend'),
+		var legend1 = L.control({position: 'bottomright'});
+		var legend2 = L.control({position: 'bottomright'});
+		var legend3 = L.control({position: 'bottomright'});
+		var div1 = L.DomUtil.create('div', 'info legend');
+		var div2 = L.DomUtil.create('div', 'info legend');
+		var div3 = L.DomUtil.create('div', 'info legend');
+					
+		legend1.onAdd = function (map) {
+
+
+	     //var div = L.DomUtil.create('div', 'info legend'),
+		 var 
 		 grades = [0, 20, 40, 80, 100, 120, 130, 150],
-		 labels = [],
+		 labels = ['<strong> YEAR 2015 </strong>'],
 		 from, to;
-
-	     for (var i = 0; i < grades.length; i++) {
+		 for (var i = 0; i < grades.length; i++) {
 		 from = grades[i];
 		 to = grades[i + 1];
 
 		 labels.push(
-		     '<i style="background:' + getColor2(from + 1,_r) + '"></i> ' +
+		     '<i style="background:' + getColor2(from + 1,'2015') + '"></i> ' +
 		     from + (to ? '&ndash;' + to : '+'));
 	     }
 
-	     div.innerHTML = labels.join('<br>');
-		 //div.innerHTML = '<select><option>1</option><option>2</option><option>3</option></select>';
-		 //div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
-	     return div;
-	 };
+	   
+		 div1.innerHTML = labels.join('<br>');
+		
+	     return div1;
+	    };
+		
+		 legend1.addTo(map);
+		 
+		 legend2.onAdd = function (map) {
 
-	 legend.addTo(map);
 
-    }
+	     //var div = L.DomUtil.create('div', 'info legend'),
+		 var 
+		 grades = [0, 20, 40, 80, 100, 120, 130, 150],
+		 labels = ['<strong> YEAR 2016 </strong>'],
+		 from, to;
+		 for (var i = 0; i < grades.length; i++) {
+		 from = grades[i];
+		 to = grades[i + 1];
+
+		 labels.push(
+		     '<i style="background:' + getColor2(from + 1,'2016') + '"></i> ' +
+		     from + (to ? '&ndash;' + to : '+'));
+	     }
+
+	   
+		 div2.innerHTML = labels.join('<br>');
+		
+	     return div2;
+	    };
+		
+		 legend2.addTo(map);
+		 legend3.onAdd = function (map) {
+
+
+	     //var div = L.DomUtil.create('div', 'info legend'),
+		 var 
+		 grades = [0, 20, 40, 80, 100, 120, 130, 150],
+		 labels = ['<strong> YEAR 2017 </strong>'],
+		 from, to;
+		 for (var i = 0; i < grades.length; i++) {
+		 from = grades[i];
+		 to = grades[i + 1];
+
+		 labels.push(
+		     '<i style="background:' + getColor2(from + 1,'2017') + '"></i> ' +
+		     from + (to ? '&ndash;' + to : '+'));
+	     }
+
+	   
+		 div3.innerHTML = labels.join('<br>');
+		
+	     return div3;
+	    };
+		
+		 legend3.addTo(map);
+				
+		
+		
+		};
+		
+
+    
 };
 xhr1.send();
